571c2f55eeaaa8f2cf38dd91a84324232b5f0fe0
DakshXSharma <sharmadaksh2315@gmail.com>
2026-05-22 11:57:56 +0530

    csv and sqlite db

---
+++ sqlite.py
@@ -0,0 +1,58 @@
+# import sqlite3
+# conn = sqlite3.connect("db1.db")
+
+# sql = '''insert into emp(name, mob, city) values('Daksh', 843728938, 'Jaipur')'''
+
+# conn.execute(sql)
+# conn.commit()
+# conn.close()
+
+
+
+# import sqlite3
+# conn = sqlite3.connect("db1.db")
+
+# sql = 'select * from emp'
+
+# res = conn.execute(sql)
+# for row in res:
+#     print(row)
+
+# conn.commit()
+# conn.close()
+
+
+
+# import sqlite3
+# conn = sqlite3.connect("db1.db")
+
+# sql = '''delete from emp'''
+
+# conn.execute(sql)  
+# conn.commit()
+# conn.close()
+
+
+
+# import sqlite3
+# conn = sqlite3.connect("db1.db")
+
+# sql = '''insert into emp(name, mob, city) values('Hudhs', 6543859, 'Kota')'''
+
+# conn.execute(sql)
+# conn.commit()
+# conn.close()
+
+
+
+# import sqlite3
+# conn = sqlite3.connect("db1.db")
+
+# sql = 'select * from emp order by name desc'
+
+# res = conn.execute(sql)
+# for row in res:
+#     print(row)
+
+# conn.commit()
+# conn.close()
